f8b76c84ff7c6d4935100eac2ca923742573cac6
Beckham (debian) <beckham.lawrence04@gmail.com>
2026-07-25 15:54:10 +0200

    docker conf ok and initial version working

---
+++ templates/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Guild Master</title>
+</head>
+<body>
+    <h1>Guild Master -- Beckham Lawrence</h1>
