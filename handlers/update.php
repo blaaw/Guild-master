18b647f657a7101851080f3911656bb2db4f8031
Beckham (personal wsl 2) <beckham.lawrence04@gmail.com>
2026-08-27 22:50:24 +0200

    Error bug fixed 4 no more dups names when update name

--- handlers/update.php
+++ handlers/update.php
@@ -1,25 +1,32 @@
 <?php
-include( __DIR__ . "/../includes/db.php");
-function updateCharacter($charname, $field, $newvalue) {
+include(__DIR__ . "/../includes/db.php");
+function updateCharacter($charname, $field, $newvalue)
+{
     $characters = getCharacters();
 
-    foreach($characters as &$c) { // the & is to pass by reference instead of value like C
+    foreach ($characters as &$c) { // the & is to pass by reference instead of value like C
 
         if ($charname === $c["name"] || $charname === strtolower($c["name"])) {
-            $c["$field"] = $newvalue;
-             
-            if (saveCharacters($characters)) {
-                echo "Character Actualizado exitosamente! <br>"; 
-                return;
+            $eaval_validCharName = findCharacter(trim($newvalue));
+
+            if ($eaval_validCharName != "") {
+                echo "Error: ya existe un personaje con ese nombre.";
             } else {
-                echo "Ha habido un error intentando actualizar.";
-                return;
+                $c["$field"] = $newvalue;
+
+                if (saveCharacters($characters)) {
+                    echo "Character Actualizado exitosamente! <br>";
+                    return;
+                } else {
+                    echo "Ha habido un error intentando actualizar.";
+                    return;
+                }
             }
-        } 
-    } 
+        }
+    }
     echo "Character no encontrado.";
 }
 
 if ($_POST) {
-    updateCharacter($_POST["char-name"], $_POST["field"], $_POST["new-value"]);   
+    updateCharacter($_POST["char-name"], $_POST["field"], $_POST["new-value"]);
 }
